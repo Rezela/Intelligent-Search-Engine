9591bd81ced76ffd5a866fb62ac441231382627c
Rezela <2558254794@qq.com>
2025-11-04 20:07:44 +0800

    封装延迟加载不同语言embedding模型的函数，统一调用接口

--- embedding.py
+++ embedding.py
@@ -1,18 +1,31 @@
 from sentence_transformers import SentenceTransformer
 from typing import List
 
-english_embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-chinese_embedding_model = SentenceTransformer('shibing624/text2vec-base-chinese')
+# english_embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+# chinese_embedding_model = SentenceTransformer('shibing624/text2vec-base-chinese')
+
+
+# embedding模型全局缓存
+embedding_cache = {}
+embedding_models = {
+    'English': "sentence-transformers/all-MiniLM-L6-v2",  # 384 dimensional
+    'Chinese': "shibing624/text2vec-base-chinese"  # 768 dimensional
+}
+
+# Lazy loading embedding model
+def get_embedding_model(language: str):
+    if language not in embedding_cache:
+        if language not in embedding_models:
+            raise ValueError(f"Unsupported language: {language}")
+        # 初始化 embedding
+        embedding_cache[language] = SentenceTransformer(embedding_models[language])
+    return embedding_cache[language]
+
 
 # 转换为embedding向量
 def embed_chunk(chunk: str, language : str= 'English') -> List[float]:
-    if language == 'English':
-        embed = english_embedding_model.encode(chunk, normalize_embeddings=True)
-    elif language == 'Chinese':
-        embed = chinese_embedding_model.encode(chunk, normalize_embeddings=True)
-    else:
-        # 处理不支持的语言，使用默认的英文模型
-        embed = english_embedding_model.encode(chunk, normalize_embeddings=True)
+    embedding_model = get_embedding_model(language)
+    embed = embedding_model.encode(chunk, normalize_embeddings=True)
     return embed.tolist()
 
 
